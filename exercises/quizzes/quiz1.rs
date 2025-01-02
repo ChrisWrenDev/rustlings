851fb45d6262104149684d467a83121b8077efac
Chris Wren <chriswrendeveloper@gmail.com>
2025-01-02 19:58:50 +0000

    Quiz 1

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -10,10 +10,18 @@
 
 // TODO: Write a function that calculates the price of an order of apples given
 // the quantity bought.
-// fn calculate_price_of_apples(???) -> ??? { ??? }
+
+fn calculate_price_of_apples(num:u32) -> u32 { 
+    if num > 40 {
+        return num * 1
+    }
+
+    num * 2
+}
 
 fn main() {
     // You can optionally experiment here.
+    calculate_price_of_apples(54);
 }
 
 // Don't change the tests!
